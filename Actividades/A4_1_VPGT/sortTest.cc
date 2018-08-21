1ee2f273e48974bb241cdf954d7c13c5281772b4
Diego Kaleb Valenzuela Carrillo <a01018992@itesm.mx>
2018-08-21 15:43:49 -0500

    Update sortTest.cc

--- Actividades/A4_1_VPGT/sortTest.cc
+++ Actividades/A4_1_VPGT/sortTest.cc
@@ -1,1 +1,24 @@
+#include "gtest/gtest.h"
+#include "sort.h"
 
+template <class T>
+class SortFixure : public ::testing::TestWithParam<T>{
+    public:
+    void SetUp(){
+        arr = new T;
+    }
+    void TearDown(){
+        delete arr;
+    }
+
+    protected:
+    T* arr;
+};
+
+typedef ::testing::Types<QuickSort> implementations;
+TYPED_TEST_CASE(SortFixure, implementations);
+TYPED_TEST(TypedFixure, implementations){
+    this -> arr -> ordenar();
+}
+
+int arr[] = {10, 7, 8, 9, 1, 5};
