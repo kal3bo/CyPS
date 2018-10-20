f4519189709372addce341ac193b69226090093f
Diego Kaleb Valenzuela Carrillo <a01018992@itesm.mx>
2018-10-19 22:43:42 -0500

    Update trazaTuRuta.py

--- Actividades/A14_2_TRAZATURUTA/trazaTuRuta.py
+++ Actividades/A14_2_TRAZATURUTA/trazaTuRuta.py
@@ -8,6 +8,7 @@ driver = webdriver.Chrome(chrome_path)
 driver.get("http://app.sct.gob.mx/sibuac_internet/ControllerUI?action=cmdEscogeRuta")
 
 driver.find_element_by_css_selector("#headerEPN > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(5) > td > a").click()
+driver.find_element_by_css_selector("#headerEPN > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(14) > td > a").click()
 
 obj = Select(driver.find_element_by_name("edoOrigen"))
 obj.select_by_index(9)
@@ -22,9 +23,33 @@ obj = Select(driver.find_element_by_name("ciudadDestino"))
 obj.select_by_index(22)
 
 obj = Select(driver.find_element_by_name("edoIntermedio1"))
-obj.select_by_index(random.randint(0, 32))
+obj.select_by_index(1)
 
 obj = Select(driver.find_element_by_name("ciudadIntermedia1"))
 obj.select_by_index(1)
 
+obj = Select(driver.find_element_by_name("tamanioVehiculo"))
+obj.select_by_index(2)
+
+obj = Select(driver.find_element_by_name("desplazamiento"))
+obj.select_by_index(3)
+
+obj = Select(driver.find_element_by_name("combustible"))
+obj.select_by_index(1)
+
+driver.find_element_by_css_selector("#headerEPN > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(16) > td:nth-child(2) > input").clear()
+driver.find_element_by_css_selector("#headerEPN > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(16) > td:nth-child(2) > input").send_keys("8.36")
 driver.find_element_by_css_selector("#headerEPN > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(4) > td.abacentro > input").click()
+
+costoTotalCasetas = driver.find_element_by_css_selector("#tContenido > tbody > tr:nth-child(47) > td:nth-child(5)")
+costoTotalCombustible = driver.find_element_by_css_selector("#tContenido > tbody > tr:nth-child(49) > td:nth-child(5)")
+costoTotal = driver.find_element_by_css_selector("#tContenido > tbody > tr:nth-child(51) > td:nth-child(5)")
+distanciaTotal = driver.find_element_by_css_selector("#tContenido > tbody > tr:nth-child(47) > td:nth-child(2)")
+tiempoTotal = driver.find_element_by_css_selector("#tContenido > tbody > tr:nth-child(47) > td:nth-child(3)")
+
+
+print("El costo total en casetas es de: ${}".format(costoTotalCasetas.text))
+print("El costo total en combistible es de: ${}".format(costoTotalCombustible.text))
+print("El costo total es de: ${}".format(costoTotal.text))
+print("La distancia total es de: {}km".format(distanciaTotal.text))
+print("El tiempo total es de: {}hrs".format(tiempoTotal.text))
